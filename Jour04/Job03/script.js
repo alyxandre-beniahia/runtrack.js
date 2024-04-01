53790d4ece751feb9730a939800eee181ef7d34f
alyxandre-beniahia <alyxandre.beniahia@laplateforme.io>
2024-04-01 20:10:50 +0200

    added type and id filter and some styling the pokemon exercice

--- Jour04/Job03/script.js
+++ Jour04/Job03/script.js
@@ -1,9 +1,17 @@
+const inputNameElement = document.getElementById("name_input");
+const inputIdElement = document.getElementById("id_input");
+const selectTypeElement = document.getElementById("type_select");
+const pokemonContainer = document.getElementById("pokemon_container");
+const filterForm = document.getElementById("pokemon_filter_form");
+
 function removeDuplicates(data) {
   return data.filter((value, index) => data.indexOf(value) == index);
 }
+
 function isNumber(char) {
   return /\d/.test(char);
 }
+
 async function getPokeType() {
   try {
     const response = await fetch("./pokemon.json");
@@ -23,49 +31,89 @@ async function getPokeType() {
     throw error;
   }
 }
+
 getPokeType();
 
 let pokemonData = null;
-
-async function getAllPokemons(filterName = "", filterId = "") {
+/**
+ * Retrieves data from a JSON file named "pokemon.json" using the fetch API.
+ * If the pokemonData variable is null, it makes a request to fetch the JSON file.
+ * It then converts the response to JSON format and assigns it to the pokemonData variable.
+ * Finally, it returns the pokemonData array.
+ * @returns {Promise<Array>} A Promise that resolves to the pokemonData array.
+ */
+async function getAllPokemons() {
   try {
     if (!pokemonData) {
       const response = await fetch("./pokemon.json");
       pokemonData = await response.json();
+      return pokemonData;
     }
-
-    const container =
-      document.querySelector(".pokemon-container") ||
-      document.body.appendChild(document.createElement("div"));
-    container.classList.add("pokemon-container");
-    container.textContent = "";
-
-    const filteredData = pokemonData.filter((item) => {
-      const nameMatch = item.name.french
-        .toLowerCase()
-        .includes(filterName.toLowerCase());
-      const idMatch = String(item.id) == filterId;
-
-      return (filterName === "" || nameMatch) && (filterId === "" || idMatch);
-    });
-
-    filteredData.forEach((item) => {
-      const pokemonHTML = `<div>${JSON.stringify(item.name.french)}</div>`;
-      container.insertAdjacentHTML("beforeend", pokemonHTML);
-    });
   } catch (error) {
     console.error("Error:", error);
     return Promise.reject(error);
   }
 }
 
-const inputNameElement = document.getElementById("name_input");
-const inputIdElement = document.getElementById("id_input");
-inputIdElement.addEventListener("input", (event) => {
-  console.log(event.target.value);
-  getAllPokemons((filterId = inputIdElement.value));
-});
-inputNameElement.addEventListener("input", (event) => {
-  console.log(event.target.value);
-  getAllPokemons((filterName = inputNameElement.value));
+/**
+ * Filters an array of pokemons based on their id, name, and type.
+ * @param {Array} pokemons - An array of pokemons.
+ * @param {string|boolean} id - The id to filter by. Defaults to false.
+ * @param {string|boolean} name - The name to filter by. Defaults to false.
+ * @param {string|boolean} type - The type to filter by. Defaults to false.
+ * @returns {Array} - An array of pokemons that match the specified filters.
+ */
+function filterPokemons(pokemons, id = false, name = false, type = false) {
+  return pokemons.filter((pokemon) => {
+    const matchId = !id || pokemon.id.toString() === id;
+    const matchName =
+      !name || pokemon.name.french.toLowerCase().includes(name.toLowerCase());
+    const matchType = !type || pokemon.type.includes(type);
+    return matchId && matchName && matchType;
+  });
+}
+getAllPokemons().then((data) => {
+  let name = "";
+  let id = "";
+  let type = "";
+
+  const updatePokemonList = () => {
+    const filteredPokemons = filterPokemons(data, id, name, type);
+    pokemonContainer.innerHTML = "";
+    filteredPokemons.forEach((pokemon) => {
+      const pokeCard = document.createElement("div");
+      pokeCard.setAttribute("class", "pokemon__card");
+      const pokeName = document.createTextNode(pokemon.name.french);
+      const nameElement = document.createElement("h3");
+      const statsElement = document.createElement("ul");
+      nameElement.appendChild(pokeName);
+      for (const [key, value] of Object.entries(pokemon.base)) {
+        const liElement = document.createElement("li");
+        // add class so color of h3 matchs Type
+        const statP = document.createTextNode(`${key}: ${value}`);
+        liElement.appendChild(statP);
+        statsElement.appendChild(liElement);
+      }
+      pokeCard.appendChild(nameElement);
+      pokeCard.appendChild(statsElement);
+      pokemonContainer.appendChild(pokeCard);
+    });
+  };
+
+  inputNameElement.addEventListener("input", (event) => {
+    name = event.target.value.trim();
+    updatePokemonList();
+  });
+
+  inputIdElement.addEventListener("input", (event) => {
+    id = event.target.value.trim();
+    updatePokemonList();
+  });
+
+  selectTypeElement.addEventListener("change", (event) => {
+    type = event.target.value;
+    updatePokemonList();
+  });
+
+  updatePokemonList();
 });
